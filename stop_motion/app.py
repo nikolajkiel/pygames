dd4bee409d72f8d8a55e6d1e509f33285d1d27cf
Nikolaj Kiel <nki@blaest.com>
2020-08-27 19:58:34 +0200

    stop motion added

---
+++ stop_motion/app.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 13:28:46 2020
+
+@author: nki
+"""
+
+
+
+
+from PyQt5 import QtGui, QtCore, QtWidgets
+import cv2
+import sys, glob, os
+from pathlib import Path
+cwd = Path(os.getcwd())
+
+
+ip = 'http://83.91.176.250/mjpg/video.mjpg'
+# cap = cv2.VideoCapture(ip)
+
+
+class DisplayImageWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None, ip=ip):
+        super().__init__(parent)
+        self.ip = 0
+
+        self.button = QtWidgets.QPushButton('Take picture')
+        self.button.clicked.connect(self.show_image)
+        self.entry_ip_cam = QtWidgets.QLineEdit(text = f'{ip}')
+        self.button_ip = QtWidgets.QPushButton('Connect to ip camera')
+        self.button_ip.clicked.connect(self.connect_ipcam)
+        self.button_save = QtWidgets.QPushButton('Save current picture')
+        self.button_save.clicked.connect(self.save_picture)
+
+        self.image_frame = QtWidgets.QLabel(text='IP camera placeholder')
+
+        self.button_left = QtWidgets.QPushButton('<')
+        self.button_left.clicked.connect(lambda: print('left'))
+        self.button_right = QtWidgets.QPushButton('>')
+        self.button_right.clicked.connect(lambda: print('right'))
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout_top_buttons = QtWidgets.QHBoxLayout()
+        self.layout.addLayout(self.layout_top_buttons)
+        self.layout_top_buttons.addWidget(self.button)
+        self.layout_top_buttons.addWidget(self.entry_ip_cam)
+        self.layout_top_buttons.addWidget(self.button_ip)
+        self.layout_top_buttons.addWidget(self.button_save)
+
+
+        self.layout.addWidget(self.image_frame)
+
+        self.layout_bottom_buttons = QtWidgets.QHBoxLayout()
+        self.layout.addLayout(self.layout_bottom_buttons)
+        self.layout_bottom_buttons.addWidget(self.button_left)
+        self.layout_bottom_buttons.addWidget(self.button_right)
+
+
+
+        self.setLayout(self.layout)
+        self.connect_ipcam()
+
+    @QtCore.pyqtSlot()
+    def show_image(self):
+        b, self.im    = self.cap.read() # cv2.imread('placeholder4.PNG')
+        if b:
+            self.image = QtGui.QImage(self.im.data, self.im.shape[1], self.im.shape[0], QtGui.QImage.Format_RGB888).rgbSwapped()
+            self.image_frame.setPixmap(QtGui.QPixmap.fromImage(self.image))
+        else:
+            print('False', b)
+
+    @QtCore.pyqtSlot()
+    def connect_ipcam(self):
+        if self.ip != self.entry_ip_cam.text():
+            self.ip = self.entry_ip_cam.text()
+            if self.ip.isdigit():
+                print('a')
+                self.cap = cv2.VideoCapture(int(self.ip))
+            else:
+                print('b')
+                self.cap = cv2.VideoCapture(self.ip)
+            self.cap.release()
+
+    @QtCore.pyqtSlot()
+    def save_picture(self):
+        cv2.imwrite('test.png', self.im)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    display_image_widget = DisplayImageWidget()
+    display_image_widget.show()
+    sys.exit(app.exec_())
--- sudoku/puzzle.py
+++ sudoku/puzzle.py
@@ -8,31 +8,34 @@ class Puzzle:
     pos = ""
 
     goal = ""
+    hest = 0
 
     def __init__(self, pos=None):
         if pos: self.pos = pos
-        
+
     def __repr__(self):
         return repr(self.pos)
-        
+
     def canonical(self):
         return repr(self)
 
     def isgoal(self):
         return self.pos == self.goal
-    
+
     def __iter__(self):
         if 0: yield self
-    
+
     def solve(self, depthFirst=False):
         queue = deque([self])
         trail = {intern(self.canonical()): None}
         solution = deque()
         load = queue.append if depthFirst else queue.appendleft
-        
+
         while not self.isgoal():
             for m in self:
-                sleep(.1)
+                sleep(.1/len(trail))
+                self.hest +=1
+                print(f'trail length: {len(trail)}, {self.hest}')
                 print(m)
                 c = m.canonical()
                 if c in trail:
@@ -40,7 +43,7 @@ class Puzzle:
                 trail[intern(c)] = self
                 load(m)
             self = queue.pop()
-        
+
         while self:
             solution.appendleft(self)
             self = trail[self.canonical()]
@@ -50,13 +53,14 @@ class Puzzle:
 class LeftToRight(Puzzle):
     pos  = [1,0,0,0,0]
     goal = [0,0,0,0,1]
-    
+
     def __iter__(self):
         for move in range(2):
             pos = deque(self.pos)
             pos.rotate(-1) if move else pos.rotate()
             yield LeftToRight(pos=list(pos))
-        
-        
-p = LeftToRight()
-results = p.solve()
+
+
+if __name__ == '__main__':
+    p = LeftToRight()
+    results = p.solve()
